d5ba969f3111f7b907aeae5a498c6efc6982cf0c
Igor Bobko <limit-speed@yandex.ru>
2021-08-02 20:23:42 +0000

    Added images.

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,15 +4,30 @@
     <p>
       Here you can find statistics for many areas. You can create your own statistical analysis and share it. <br/>
       Let's make objective statistics available to everybody.
+
+
+      <img src="http://ec2-18-218-65-252.us-east-2.compute.amazonaws.com:8080/"/>
     </p>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mounted() {
+    console.log("hello.");
+    axios.get("http://ec2-18-218-65-252.us-east-2.compute.amazonaws.com:5000").then(function (data){
+      console.log(data);
+    });
+
+
+
+
   }
 }
 </script>
