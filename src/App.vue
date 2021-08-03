0f2498d24723f236e219bf949a309539cd309cb1
Igor Bobko <limit-speed@yandex.ru>
2021-08-03 12:24:38 +0000

    .

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <MainPage msg="Statistics for all"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import MainPage from './components/MainPage.vue'
-
-export default {
-  name: 'App',
-  components: {
-    MainPage
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
